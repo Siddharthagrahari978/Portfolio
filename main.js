e1589849be5df052a44acab13093b78e0fec5fe7
Siddharth Kumar <43567482+Siddharthagrahari978@users.noreply.github.com>
2020-07-21 14:48:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.onreadystatechange = function() {
 		document.querySelector("#loader").style.visibility = "visible"; 
 	} else if(document.readyState == "complete") { 
 		var timeTaken=(Date.now()-timerStart)/1000;
-		var loadingTime = 3000-timeTaken;
+		var loadingTime = 2000-timeTaken;
 		console.log(timeTaken,loadingTime);
 		window.setTimeout(function(){
 			document.querySelector("#loader").style.display = "none"; 
