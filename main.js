4e313b4ab91b44ee64a0e8c03ca1cd1b62b2f9de
Siddharth Kumar <43567482+Siddharthagrahari978@users.noreply.github.com>
2020-07-21 15:12:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,49 +12,50 @@ document.onreadystatechange = function() {
 		window.setTimeout(function(){
 			document.querySelector("#loader").style.display = "none"; 
 			document.querySelector("body").style.visibility = "visible"; 
+			// Function expression to select elements
+			const selectElement = (s) => document.querySelector(s);
+			const navLinks = document.querySelectorAll(".nav-link");
+
+			selectElement(".burger-menu-icon").addEventListener("click", () => {
+			    // Toggle the nav on menu icon click
+			    selectElement(".nav-list").classList.toggle("active");
+
+			    // Animate Links
+			    navLinks.forEach((link, index) => {
+				if (link.style.animation) {
+				    link.style.animation = "";
+				} else {
+				    link.style.animation = `navLinkFade 0.5s ease forwards ${
+					index / 7 + 0.5
+					}s`;
+				}
+			    });
+
+			    //Burger Animation
+			    selectElement(".burger-menu-icon").classList.toggle("toggle");
+			});
+
+			//close navbar on nav link click
+
+			navLinks.forEach((link) => {
+			    link.addEventListener("click", () => {
+				selectElement(".nav-list").classList.toggle("active");
+
+				navLinks.forEach((link, index) => {
+				    if (link.style.animation) {
+					link.style.animation = "";
+				    } else {
+					link.style.animation = `navLinkFade 0.5s ease forwards ${
+					    index / 7 + 0.5
+					    }s`;
+				    }
+				});
+
+				selectElement(".burger-menu-icon").classList.toggle("toggle");
+			    });
+			});
+
 		},loadingTime);
 	} 
 };
 
-// Function expression to select elements
-const selectElement = (s) => document.querySelector(s);
-const navLinks = document.querySelectorAll(".nav-link");
-
-selectElement(".burger-menu-icon").addEventListener("click", () => {
-    // Toggle the nav on menu icon click
-    selectElement(".nav-list").classList.toggle("active");
-
-    // Animate Links
-    navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = "";
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${
-                index / 7 + 0.5
-                }s`;
-        }
-    });
-
-    //Burger Animation
-    selectElement(".burger-menu-icon").classList.toggle("toggle");
-});
-
-//close navbar on nav link click
-
-navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-        selectElement(".nav-list").classList.toggle("active");
-
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${
-                    index / 7 + 0.5
-                    }s`;
-            }
-        });
-
-        selectElement(".burger-menu-icon").classList.toggle("toggle");
-    });
-});
