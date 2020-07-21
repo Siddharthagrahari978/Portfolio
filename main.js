c36f63d2820b5bb8cff45391333f126753167cfd
Siddharth Kumar <43567482+Siddharthagrahari978@users.noreply.github.com>
2020-07-21 14:15:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // For Loader
 document.onreadystatechange = function() { 
-	if (document.readyState !== "complete" && !setInterval(()=>{return true;},3000)) { 
+	if (document.readyState !== "complete") { 
 		document.querySelector("body").style.visibility = "hidden"; 
 		document.querySelector("#loader").style.visibility = "visible"; 
-	} else { 
+	} else if(document.readyState == "complete") { 
 		document.querySelector("#loader").style.display = "none"; 
 		document.querySelector("body").style.visibility = "visible"; 
 	} 
