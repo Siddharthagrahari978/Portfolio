0243bf1d2a285176080ce95b9bd08f1a1290c1b2
Siddharth Kumar <43567482+Siddharthagrahari978@users.noreply.github.com>
2020-07-22 10:24:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,61 +1,42 @@
-// For Loader
-var timerStart = Date.now();
-document.onreadystatechange = function() { 
-	if (document.readyState !== "complete") { 
-		document.querySelector("body").style.visibility = "hidden"; 
-		document.querySelector("#loader").style.visibility = "visible"; 
-	} else if(document.readyState == "complete") { 
-		var timeTaken=(Date.now()-timerStart)/1000;
-		if(timeTaken<1500){var loadingTime = 1500-timeTaken;}
-		else{var loadingTime = timeTaken;}
-		console.log(timeTaken,loadingTime);
-		window.setTimeout(function(){
-			document.querySelector("#loader").style.display = "none"; 
-			document.querySelector("body").style.visibility = "visible"; 
-			// Function expression to select elements
-			const selectElement = (s) => document.querySelector(s);
-			const navLinks = document.querySelectorAll(".nav-link");
+// Function expression to select elements
+const selectElement = (s) => document.querySelector(s);
+const navLinks = document.querySelectorAll(".nav-link");
 
-			selectElement(".burger-menu-icon").addEventListener("click", () => {
-			    // Toggle the nav on menu icon click
-			    selectElement(".nav-list").classList.toggle("active");
+selectElement(".burger-menu-icon").addEventListener("click", () => {
+    // Toggle the nav on menu icon click
+    selectElement(".nav-list").classList.toggle("active");
 
-			    // Animate Links
-			    navLinks.forEach((link, index) => {
-				if (link.style.animation) {
-				    link.style.animation = "";
-				} else {
-				    link.style.animation = `navLinkFade 0.5s ease forwards ${
-					index / 7 + 0.5
-					}s`;
-				}
-			    });
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.5
+                }s`;
+        }
+    });
 
-			    //Burger Animation
-			    selectElement(".burger-menu-icon").classList.toggle("toggle");
-			});
+    //Burger Animation
+    selectElement(".burger-menu-icon").classList.toggle("toggle");
+});
 
-			//close navbar on nav link click
+//close navbar on nav link click
 
-			navLinks.forEach((link) => {
-			    link.addEventListener("click", () => {
-				selectElement(".nav-list").classList.toggle("active");
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        selectElement(".nav-list").classList.toggle("active");
 
-				navLinks.forEach((link, index) => {
-				    if (link.style.animation) {
-					link.style.animation = "";
-				    } else {
-					link.style.animation = `navLinkFade 0.5s ease forwards ${
-					    index / 7 + 0.5
-					    }s`;
-				    }
-				});
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${
+                    index / 7 + 0.5
+                    }s`;
+            }
+        });
 
-				selectElement(".burger-menu-icon").classList.toggle("toggle");
-			    });
-			});
-
-		},loadingTime);
-	} 
-};
-
+        selectElement(".burger-menu-icon").classList.toggle("toggle");
+    });
+});
